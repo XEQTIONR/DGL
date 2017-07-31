822ab36f7886d8326b13974df43d69016aec7a03
Ishtehar Hussain <ishteharhussain@gmail.com>
2017-07-31 18:14:23 -0400

    Move the main menu to a partial file

---
+++ resources/views/partials/main_menu.blade.php
@@ -0,0 +1,32 @@
+<div class="container-fluid">
+  <div class="navbar-header">
+
+    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+
+
+    <a class="navbar-brand" href="#">
+      <img alt="Brand" src="img/site-1-a.png" height="80">
+    </a>
+  </div>
+
+  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+    <ul class="nav navbar-nav navbar-left">
+      <li><a href="#">news</a></li>
+      <li><a href="#">tournaments</a></li>
+      <li><a href="#">Teams</a></li>
+      <li><a href="#">Media</a></li>
+      <li><a href="#">About</a></li>
+      {{--<li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>--}}
+    </ul>
+
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#">login</a></li>
+      <li><a href="#">register</a></li>
+    </ul>
+  </div><!-- /.navbar-collapse -->
+</div>
